5293378f128aa1ff14ad439dde347be4582ba733
nate.french <Guest@epicodus-2.local>
2017-03-29 20:46:09 -0700

    values update on every button click. Winner chosen after 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+//Business Logic
 function Player(playerName){
   this.playerName = playerName;
   this.roll = 0;
@@ -6,72 +6,67 @@ function Player(playerName){
   this.tracker = 0;
 }
 
-
 Player.prototype.hold = function(){
   this.total+= this.roll;
   this.roll = 0;
-  $("#totalScore").text(this.total);
-  console.log("Total score is " + this.total);
 }
 
 Player.prototype.addTotal = function(dice){
   this.roll+= dice;
   this.tracker++;
-  $("#tracker").text(this.tracker);
-  $("#score").text(this.roll);
-  if ( this.total + this.roll  >=  100){
+  if (this.total + this.roll >= 100){
     $(".win").show();
     $("#totalScore").text(this.total+= this.roll);
   }
-  console.log("Roll total is " + this.roll);
 }
 
 Player.prototype.clear = function(){
   this.roll = 0;
 }
 
-
 var diceRolls = function() {
   min = Math.ceil(1);
   max = Math.floor(6);
   return Math.floor(Math.random() * (max - min)) + min;
-
 }
 
 
 var generator = function(player) {
     var dice = diceRolls();
-    console.log("Dice amount is " + dice);
     if (dice === 1) {
       player.clear();
-      console.log("clear run");
     }
     else {
       player.addTotal(dice);
     }
 }
 
+var users =[];
+
 var createUser = function(name) {
+    $(".name").text(name);
     return new Player(name);
-    console.log(name);
 }
 
+var updateText = function(name) {
+    $("#score").text(name.roll);
+    $("#tracker").text(name.tracker);
+    $("#totalScore").text(name.total);
+}
 
+//User Interface
 $(document).ready(function() {
-
   $("form").submit(function(event){
     event.preventDefault();
     var input = $("#name").val();
-    var name = createUser(input);
-    console.log(name);
+    var name = createUser(input);j
     $("#rollDice").click(function() {
       generator(name);
-
-
+      updateText(name);
     });
     $("#holdDice").click(function() {
       name.hold();
-
+      updateText(name);
     });
   });
 });
